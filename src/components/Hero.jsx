46e201532957e7f562f8745d0e37ca4cb87ce4e4
tezena <tewodrosnibretadal2012@gmail.com>
2023-08-02 15:13:49 -0700

    hero section for gallery page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, FormControl, Button } from 'react-bootstrap';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 
 const HeroSection = () => {
   return (
-      <div className="hero-section h-50 text-white d-flex align-items-center hero-background" style={{
+      <div className="hero-section h-50 text-white d-flex align-items-center hero-background mb-5" style={{
           backgroundImage: 'url(assets/images/img4.jpg)', opacity: 1,
           backgroundPosition: "center", backgroundSize:"cover" }}>
-      <Container>
-        <Row className="justify-content-center">
-          <Col xs={12} md={8} lg={6} xl={5}>
-            <h1 className="hero-title display-4">Welcome to My Website</h1>
-            <h2 className="hero-subtitle">Discover Amazing Content</h2>
-            <Form className="hero-search">
-              <Row className="align-items-center">
-                <Col xs={8} md={9}>
-                  <Form.Control type="text" placeholder="Search..." className="search-input" />
-                </Col>
-                <Col xs={4} md={3}>
-                  <Button className="search-button">Search</Button>
-                </Col>
-              </Row>
-            </Form>
-            <h5 className="hero-description">Get Started Today!</h5>
-          </Col>
+      <Container className='mt-o'>
+        <Row className="justify-content-center w-100">
+            <h1 className="hero-title display-4 w-100 fw-normal">Art That Speaks to Your Soul</h1>
+            <h4 className="hero-subtitle w-100 fw-light">Discover the Unseen, Celebrate the Unconventional</h4>
+            <InputGroup className="mb-3 w-50 mt-2">
+            <Form.Control
+          placeholder=""
+          aria-label="search"
+                          aria-describedby="basic-addon2"
+                          className='bg-white bg-opacity-50 border border-2'
+        />
+        <Button variant="warning text-light" id="button-addon2"  className='fw-semibold'>
+          Search
+        </Button>
+      </InputGroup>
+            <h6 className="fw-light px-5">Immerse yourself in an extraordinary collection of artworks that resonate with your deepest emotions. </h6>
         </Row>
       </Container>
     </div>
