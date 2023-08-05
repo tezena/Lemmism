7f06d2a92e319f2e38c2aef6f1c7e7ccd79711ba
Fita-Wegene <fitaawd@gmail.com>
2023-08-05 23:21:40 +0300

    Blog Page added

--- src/components/Blog/SideEvents.jsx
+++ src/components/Blog/SideEvents.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SideEvents() {
   return (
-    <div className='bg-primary text-light p-2 m-2'>
+    <div className='bg-primary text-light p-2 m-2 d-md-block'>
         <h2>Event title</h2>
         <p>some desciription</p>
     </div>
