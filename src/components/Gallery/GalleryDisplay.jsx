8f8a51a4455f635a247a9640545e6326b81b6412
tezena <tewodrosnibretadal2012@gmail.com>
2023-08-10 13:08:05 -0700

    add motion div

--- src/components/Gallery/GalleryDisplay.jsx
+++ src/components/Gallery/GalleryDisplay.jsx
@@ -5,6 +5,10 @@ import Masonry from 'react-masonry-css';
 import './Gallery.css'
 import images from "../imges";
 import Hove from "./Hove";
+import  { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 
 function GalleryDisplay (){
   const [currentImage, setCurrentImage] = React.useState(null);
@@ -24,27 +28,45 @@ function GalleryDisplay (){
     500: 1
   };
 
+
+
+  // for animation
+  const picVariants = {
+    visible: { opacity: 1, transition: { duration: 1 } },
+    hidden: { opacity: 0.8, }
+  };
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
   return (
-    <div className="px-5  ">
+
+    <div  className="px-5  ">
       <Masonry
         breakpointCols={breakpointColumnsObj}
         className="my-masonry-grid"
         columnClassName="my-masonry-grid_column "
       >
         {images.map((image, idx) => (
-          <div
+          <motion.div
+          ref={ref}
+          animate={controls}
+          initial="hidden"
+          variants={picVariants}
             key={idx}
             className="image-container"
             onMouseOver={() => handleMouseOver(image)}
             onMouseOut={handleMouseOut}
           >
-            <img src={image.src} alt={image.alt} className="img-fluid"/>
+            
+            <img src={image.src} alt={image.alt} className="img-fluid" />
+ 
             {currentImage === image && (
               <div className="image-info">
                 <Hove/>
               </div>
             )}
-          </div>
+          </motion.div>
         ))}
       </Masonry>
     </div>
